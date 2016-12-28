4a96d9fa3258a9a95d174ca7f4a248511a6e0f7d
Gabriel Filipe <gabrielfilipe@mail.ru>
2016-12-28 01:21:49 -0200

    Adicionado Navigation Drawer em HomeActivity

--- app/src/main/java/ilist/gabrielrunescape/com/br/view/HomeActivity.java
+++ app/src/main/java/ilist/gabrielrunescape/com/br/view/HomeActivity.java
@@ -3,17 +3,21 @@ package ilist.gabrielrunescape.com.br.view;
 import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+import android.view.MenuItem;
 import android.content.Intent;
 import ilist.gabrielrunescape.com.br.R;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
 import ilist.gabrielrunescape.com.br.dao.ItemDAO;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import ilist.gabrielrunescape.com.br.adapter.ItemAdapter;
 import android.support.design.widget.FloatingActionButton;
-import ilist.gabrielrunescape.com.br.model.SimpleDividerItemDecoration;
 
 /**
  * Cria e exibe todos os elementos (views) necessários na tela inicial (HomeActivity).
@@ -22,12 +26,23 @@ import ilist.gabrielrunescape.com.br.model.SimpleDividerItemDecoration;
  * @version 1.0
  * @since 2016-12-25
  */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private ItemDAO dao;
+    private DrawerLayout drawer;
     private RecyclerView recyclerView;
+    private ActionBarDrawerToggle toggle;
     private SwipeRefreshLayout swipeContainer;
     private FloatingActionButton floatingButton;
     private static String TAG = HomeActivity.class.getSimpleName();
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     /**
@@ -38,18 +53,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         Log.i(TAG, "Criando e carregando elementos da ActivityView ...");
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        //recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         floatingButton = (FloatingActionButton) findViewById(R.id.floatingButton);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
 
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
-        swipeContainer.setColorSchemeResources(android.R.color.holo_red_light, android.R.color.holo_orange_light);
+        //recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
+        //swipeContainer.setColorSchemeResources(android.R.color.holo_red_light, android.R.color.holo_orange_light);
     }
 
     @Override
@@ -60,7 +85,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onResume();
 
         try {
-            updateData();
+            //updateData();
 
             floatingButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -77,6 +102,29 @@ public class HomeActivity extends AppCompatActivity {
         } catch (Exception ex) {
             Log.e(TAG, ex.getMessage());
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_az:
+            case R.id.nav_za:
+            case R.id.nav_add:
+            case R.id.nav_comprar:
+            case R.id.nav_comprado:
+            default:
+                Toast.makeText(this, "Função não programada", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     /**
